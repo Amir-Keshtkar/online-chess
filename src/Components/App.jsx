2b7a9e0c6bf7116986bed117dbe8626b2cb2f14e
Amir keshtkar <amir78ahkl@gmail.com>
2022-01-23 10:13:02 +0330

    adding default stages on frontendd

---
+++ src/Components/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import GameBoard from "./GameBoard";
+
+class App extends Component {
+  render() {
+    return <GameBoard />;
+  }
+}
+
+export default App;
