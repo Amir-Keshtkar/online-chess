d5cf3e38f15043c191f726350bcd67c9131ae0ed
khalatbari <hasankhalatbari08@gmail.com>
2022-01-25 22:20:57 +0330

    Add massenger & Use Express_SocketIO

---
+++ src/Components/chat.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+var socket = io.connect("http://localhost:3000");
+
+var room = document.getElementById("room");
+var feedback = document.getElementById("feedback");
+var user = document.getElementById("user");
+var message = document.getElementById("message");
+var send = document.getElementById("send");
+
+/*const [response, setResponse] = useState("");
+useEffect(() => {
+
+}, []);*/
+
+send.addEventListener("click", function () {
+  if (!user.value) {
+    alert("Please enter a username first.");
+    return;
+  }
+  if (!message.value) {
+    return;
+  }
+  socket.emit("CHAT", {
+    user: user.value,
+    message: message.value,
+  });
+});
+
+socket.on("CHAT", function (data) {
+  room.innerHTML +=
+    "<p><strong>" + data.user + ":</strong> " + data.message + "</p>";
+  feedback.style.visibility = "hidden";
+});
+
+message.addEventListener("keypress", function () {
+  if (!user.value) {
+    alert("Please enter a username first.");
+    return;
+  }
+  socket.emit("TYPING", { user: user.value });
+});
+
+socket.on("TYPING", function (data) {
+  feedback.innerHTML = "<i>" + data.user + " is typing ...</i>";
+  feedback.style.visibility = "visible";
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import {DndProvider} from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend';
-import * as serviceWorker from './serviceWorker';
 
 import Game from './Components/Game.js'
+import './Components/Style.css';
+
+
+
 
 
 ReactDOM.render(
-  <Game />,
+  <Fragment>
+    <Game />
+    <div class="main-container">
+          <h2 class="app-title">The Messenger</h2>
+          <div class="room-window">
+            <div class="room-text" id="room"></div>
+            <div class="room-feedback" id="feedback">status</div>
+          </div>
+          <input class="text-user" type="text" id="user" placeholder="username" />
+          <input
+            class="text-message"
+            type="text"
+            id="message"
+            placeholder="message"
+          />
+          <button class="send-btn" id="send">Send</button>
+        </div>
+        <script src="./Components/chat.js"></script>
+  </Fragment>,
   document.getElementById('root')
 );
