c15f7df04ae99408c3eee1a7e15dcdf8cafdf651
Amir keshtkar <amir78ahkl@gmail.com>
2022-01-26 02:34:09 +0330

    file name chengad because of the conflict

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 /*import {DndProvider} from 'react-dnd';
 import {HTML5Backend} from 'react-dnd-html5-backend';
 import * as serviceWorker from './serviceWorker';*/
-import Game from './Components/Game.jsx'
+import Game from './Components/Game.js'
 
 ReactDOM.render(
   <Game />,
